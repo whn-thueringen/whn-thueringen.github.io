c95864a9c6a287fa448dc8d60edf99f0768cb801
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-10-02 16:08:12 +0200

    Commit

--- src/client/_scripts/index.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from 'solid-js/web';
-import App from './app.tsx';
-
-const rootElement = document.getElementById('Kontakt');
-if (rootElement) {
-    render(() => <App />, rootElement);
-}
