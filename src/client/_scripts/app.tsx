5112b57a66c9f4a208169d1f89d331cf56abb39a
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-10-14 18:52:14 +0200

    app.tsx

--- src/client/_scripts/app.tsx
+++ src/client/_scripts/app.tsx
@@ -1,1 +1,31 @@
+import { render } from "solid-js/web";
+import type { JSX } from "solid-js";
+import Impressum from "./impressum.tsx";
+import Header from "./header.tsx";
+import Navbar from "./navbar.tsx";
+import Leitthemen from "./leitthemen.tsx";
+import SponsorSlider from "./sponsor_slider.tsx";
+import News from "./news.tsx";
+import Team from "./team.tsx"; 
 
+function App(): JSX.Element {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Leitthemen />
+      <SponsorSlider />
+      <News />
+      <Team />
+      <Impressum />
+    </>
+  );
+}
+
+const root = document.getElementById("app");
+
+if (root) {
+  render(() => <App />, root);
+} else {
+  console.error("Root element not found");
+}
