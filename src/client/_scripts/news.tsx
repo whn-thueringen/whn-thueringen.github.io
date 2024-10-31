e73cf2a625dcc8f279e3ad9e6d9b71197bfd0d31
Jonas <jode13@web.de>
2024-10-31 09:39:42 +0100

    improved readability

--- src/client/_scripts/news.tsx
+++ src/client/_scripts/news.tsx
@@ -1,111 +1,73 @@
 import WaveTop from "./wave_top.tsx";
 import WaveBottom from './wave_bottom.tsx';
 
-interface CardProps {
-  title: string;
-  date: string;
-  description: string;
+import { For } from 'solid-js';
 
-  imgSrc1: string;
-  imgSrc2: string;
-  imgSrc3: string;
-
-  imgSrc1Alt: string;
-  imgSrc2Alt: string;
-  imgSrc3Alt: string;
-  
-  id: string;
-  target: string;
-  modalId1: string;
-  modalId2: string;
-  modalId3: string;
+interface Image {
+  src: string;
+  alt: string;
+  modalId: string;
 }
 
-const NewsCard1Image = (props: CardProps) => (
-  <>
-    <div class="col-md-5 mb-1">
-      <div class="timeline-date px-4">{props.date}</div>
-      <div class="card">
-          <div id={props.id} class="carousel slide" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                  <div class="carousel-item active">
-                      <img src={props.imgSrc1} class="card-img-top" alt={props.imgSrc1Alt} data-bs-toggle="modal" data-bs-target={props.modalId1} loading="lazy"></img>
-                  </div>
-              </div>
-          </div>
-          <div class="card-body">
-              <h5 class="card-title">{props.title}</h5>
-              <p class="card-text">{props.description}</p>
-          </div>
-      </div>
-  </div>
-  </>
-);
+interface CardProps {
+  id: string;
+  date: string;
+  title: string;
+  description: string;
+  images: Image[];
+  target: string;
+}
 
-const NewsCard2Images = (props: CardProps) => (
+const NewsCard = (props: CardProps) => (
   <>
     <div class="col-md-5 mb-1">
       <div class="timeline-date px-4">{props.date}</div>
       <div class="card">
-          <div id={props.id} class="carousel slide" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                  <div class="carousel-item active">
-                      <img src={props.imgSrc1} class="card-img-top" alt={props.imgSrc1Alt} data-bs-toggle="modal" data-bs-target={props.modalId1} loading="lazy"></img>
-                  </div>
-                  <div class="carousel-item">
-                      <img src={props.imgSrc2} class="card-img-top" alt={props.imgSrc2Alt} data-bs-toggle="modal" data-bs-target={props.modalId2} loading="lazy"></img>
-                  </div>
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target={props.target} data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target={props.target} data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-              </button>
+        <div id={props.id} class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <For each={props.images}>
+              {(image, index) => (
+                <div class={`carousel-item ${index() === 0 ? 'active' : ''}`}>
+                  <img
+                    src={image.src}
+                    class="card-img-top"
+                    alt={image.alt}
+                    data-bs-toggle="modal"
+                    data-bs-target={image.modalId}
+                  />
+                </div>
+              )}
+            </For>
           </div>
-          <div class="card-body">
-              <h5 class="card-title">{props.title}</h5>
-              <p class="card-text">{props.description}</p>
-          </div>
+          {props.images.length > 1 && (
+            <>
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target={props.target}
+                data-bs-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target={props.target}
+                data-bs-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </>
+          )}
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{props.title}</h5>
+          <p class="card-text">{props.description}</p>
+        </div>
       </div>
     </div>
-  </>
-);
-
-const NewsCard3Images = (props: CardProps) => (
-  <>
-    <div class="col-md-5 mb-1">
-      <div class="timeline-date px-4">{props.date}</div>
-      <div class="card">
-          <div id={props.id} class="carousel slide" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                  <div class="carousel-item active">
-                      <img src={props.imgSrc1} class="card-img-top" alt={props.imgSrc1Alt} data-bs-toggle="modal" data-bs-target={props.modalId1} loading="lazy"></img>
-                  </div>
-                  <div class="carousel-item">
-                      <img src={props.imgSrc2} class="card-img-top" alt={props.imgSrc2Alt} data-bs-toggle="modal" data-bs-target={props.modalId2} loading="lazy"></img>
-                  </div>
-                  <div class="carousel-item">
-                      <img src={props.imgSrc3} class="card-img-top" alt={props.imgSrc3Alt} data-bs-toggle="modal" data-bs-target={props.modalId3} loading="lazy"></img>
-                  </div>
-              </div>
-              <button class="carousel-control-prev" type="button" data-bs-target={props.target} data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target={props.target} data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-              </button>
-          </div>
-          <div class="card-body">
-              <h5 class="card-title">{props.title}</h5>
-              <p class="card-text">{props.description}</p>
-          </div>
-      </div>
-  </div>
   </>
 );
 
@@ -122,70 +84,60 @@ const News = () => (
       <section class="news-section container text-center">
         <h2 class="text-center mb-3">News & Aktuelles</h2>
         <div class="row justify-content-center">
-          <NewsCard2Images
+        <NewsCard
               id='News1'
               target="#News1"
-              modalId1="#NewsModal6"
-              modalId2="#NewsModal7"
-              modalId3=""
-              title="Förderverein meets JUMPING FITNESS"
+              title="\u{201e}Girls Cup\u{201c} \u{1f93e}"
               date="Jeden Montag 16.00-16.45"
-              imgSrc1="./_assets/images/Förderverein_JUMPING_Fitness.webp"
-              imgSrc2="./_assets/images/Pennewiss_Fitness_Instagram.webp"
-              imgSrc3=""
-              imgSrc1Alt="Bild: Förderverein JUMPING Fitness"
-              imgSrc2Alt="Bild: Pennewiss Fitness Instagram"
-              imgSrc3Alt=""
-              description="„Richtig cool“, so das Feedback der Kinder und Eltern, die heute erstmals am JUMPING FITNESS im EASYFITNESS in Erfurt teilgenommen. Jeden Montag von 16.00-16.45 Uhr findet ein Kurs von jung bis alt statt.
-Wer Lust hat sich auszupowern und „auf Touren“ zu kommen kann sich gerne bei uns melden. Wir sind auf jeden Fall das nächste Mal wieder dabei."
-          />
-          <NewsCard3Images
+              description="Am vergangenen Sonntag fand das erste selbst organisierte  Turnier des F\u{00f6}rderverein weiblicher Handballnachwuchs Th\u{00fc}ringen e.V. statt. Sechs Mannschaften (HC Leipzig, TUS Dieskau, HSG Saalfeld und THC) traten gegen einander an und schenkten sich dabei nichts. Die mitgereisten Fans und zahlreiche Zuschauer sorgten f\u{00fc}r ordentlich Stimmung in der \u{201e}Blechb\u{00fc}chse\u{201c}. Am Ende des Tages nahm der TUS Dieskau den Pokal \u{1f3c6} mit nach Hause. Danke an alle Vereine, die die Anreise auf sich genommen haben. Fortsetzung folgt \u{1f596}."
+              images={[
+                { src: "webp", alt: "Bild", modalId: "#NewsModal6" },
+                { src: "webp", alt: "Bild", modalId: "#NewsModal7" }
+              ]}
+            />
+          <NewsCard
+              id='News1'
+              target="#News1"
+              title="F\u{00f6}rderverein meets JUMPING FITNESS"
+              date="Jeden Montag 16.00-16.45"
+              description="\u{201e}Richtig cool\u{201c}, so das Feedback der Kinder und Eltern, die heute erstmals am JUMPING FITNESS im EASYFITNESS in Erfurt teilgenommen. Jeden Montag von 16.00-16.45 Uhr findet ein Kurs von jung bis alt statt.
+Wer Lust hat sich auszupowern und \u{201e}auf Touren\u{201c} zu kommen kann sich gerne bei uns melden. Wir sind auf jeden Fall das nächste Mal wieder dabei."
+              images={[
+                { src: "./_assets/images/Förderverein_JUMPING_Fitness.webp", alt: "Bild: Förderverein JUMPING Fitness", modalId: "#NewsModal6" },
+                { src: "./_assets/images/Pennewiss_Fitness_Instagram.webp", alt: "Bild: Pennewiss Fitness Instagram", modalId: "#NewsModal7" }
+              ]}
+            />
+          <NewsCard
               id='News2'
               target="#News2"
-              modalId1="#NewsModal3"
-              modalId2="#NewsModal4"
-              modalId3="#NewsModal5"
               title="Erfolgreiche dm-markt Kassieraktion"
               date="11.10.2024 | Erfurt"
-              imgSrc1="./_assets/images/kassieraktion_bild_alle_2024.webp"
-              imgSrc2="./_assets/images/drogerie_markt_spende_check_2024.webp"
-              imgSrc3="./_assets/images/pdf_kassieraktion_2024.webp"
-              imgSrc1Alt="Bild: Kassieraktion"
-              imgSrc2Alt="Bild: Kassieraktion Check"
-              imgSrc3Alt="Bild: Pdf Kassieraktion"
               description="Ein großes Dankeschön geht an den dm-Markt im Thüringen Park Erfurt. In einer bisher einmaligen Aktion und mit Unterstützung der Bundesliga Profis Julie Holm & Josefine Hanfland sowie 12 Nachwuchshandballerinnen wurde in einer 2-stündigen Aktion eine unfassbare Summe von 5.700 EUR erwirtschaftet. Die Summe wird zu 100% an den Förderverein gespendet. DANKE an alle Beteiligten, die diese Aktion so erfolgreich gemacht haben."
+              images={[
+                { src: "./_assets/images/kassieraktion_bild_alle_2024.webp", alt: "Bild: Kassieraktion", modalId: "#NewsModal3" },
+                { src: "./_assets/images/drogerie_markt_spende_check_2024.webp", alt: "Bild: Kassieraktion Check", modalId: "#NewsModal4" },
+                { src: "./_assets/images/pdf_kassieraktion_2024.webp", alt: "Bild: Pdf Kassieraktion", modalId: "#NewsModal5" }
+              ]}
             />
-            <NewsCard1Image
+            <NewsCard
               id='News3'
-              target="#News3"   
-              modalId1="#NewsModal1"
-              modalId2=""
-              modalId3=""           
+              target="#News3"            
               title="Internationales Handballturnier Kolding 2025"
               date="19.04 - 20.04.2025 | Erfurt"
-              imgSrc1="./_assets/images/Kolding_Cup_2025.webp"
-              imgSrc2=""
-              imgSrc3=""
-              imgSrc1Alt="Bild: Kolding Cup 2025"
-              imgSrc2Alt=""
-              imgSrc3Alt=""
               description="Wir wollen den Mädchen der D-Jugend des THC die Teilnahme an einem der größten Handballturniere ermöglichen."
+              images={[
+                { src: "./_assets/images/Kolding_Cup_2025.webp", alt: "Bild: Kolding Cup 2025", modalId: "#NewsModal1" }
+              ]}
             />
-            <NewsCard1Image
+            <NewsCard
               id='News4'
-              target="#News4"  
-              modalId1="#NewsModal2"
-              modalId2=""
-              modalId3=""           
+              target="#News4"          
               title="Neue Leibchen für den Nachwuchs"
               date="2024 | Erfurt"
-              imgSrc1="./_assets/images/neue-leibchen-2024.webp"
-              imgSrc2=""
-              imgSrc3=""
-              imgSrc1Alt="Bild: Neue Leibchen 2024"
-              imgSrc2Alt=""
-              imgSrc3Alt=""
               description="Die Leibchen sind bereits im Einsatz, da die Mädels auch in den Ferien fleißig trainiert haben."
+              images={[
+                { src: "./_assets/images/neue-leibchen-2024.webp", alt: "Bild: Neue Leibchen 2024", modalId: "#NewsModal2" }
+              ]}
             />
         </div>
         <AllProjectsButton></AllProjectsButton>
--- src/client/projekte/_scripts/projekte.tsx
+++ src/client/projekte/_scripts/projekte.tsx
@@ -1,106 +1,73 @@
+import { For, Show } from 'solid-js';
 
-interface CardProps {
-  title: string;
-  date: string;
-  description: string;
-
-  link: string;
-  linkText: string;
-  linkClass: string;
-
-  imgSrc1: string;
-  imgSrc2: string;
-  imgSrc3: string;
-
-  imgSrc1Alt: string;
-  imgSrc2Alt: string;
-  imgSrc3Alt: string;
-
-  id: string;
-  target: string;
+interface Image {
+  src: string;
+  alt: string;
+  modalId: string;
 }
 
-const CardContent1Image = ( props: CardProps ) => (
+interface CardProps {
+  date: string;
+  images: Image[];
+  title: string;
+  description: string;
+  link: string;
+  linkClass: string;
+  linkText: string;
+  target: string;
+  id: string;
+}
+
+const ProjectCard = (props: CardProps) => (
   <>
     <div class="timeline-date px-4 text-center">{props.date}</div>
     <div class="card d-flex flex-column flex-md-row">
       <div class="col-md-6">
-        <img src={props.imgSrc1} alt={props.imgSrc1Alt} class="card-img-top" loading="lazy"></img>
-      </div>
-      <div class="col-md-6 d-flex flex-column justify-content-center">
-        <div class="card-body">
-          <h5 class="card-title">{props.title}</h5>
-          <p class="card-text">{props.description}</p>
-          <a href={props.link} class={props.linkClass} target="_blank">{props.linkText}</a>
-        </div>
-      </div>
-    </div>
-  </>
-);
-
-const _CardContent2Images = ( props: CardProps ) => (
-  <>
-    <div class="timeline-date px-4 text-center">{props.date}</div>
-    <div class="card d-flex flex-column flex-md-row">
-    <div id={props.id} class="carousel" data-bs-ride="carousel">
+        <div id={props.target} class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
-              <div class="carousel-item active">
-                  <img src={props.imgSrc1} class="card-img-top" alt={props.imgSrc1Alt} data-bs-toggle="modal" data-bs-target="#Kassieraktion_alle_modal" loading="lazy"></img>
-              </div>
-              <div class="carousel-item">
-                  <img src={props.imgSrc2} class="card-img-top" alt={props.imgSrc2Alt} data-bs-toggle="modal" data-bs-target="#Kassieraktion_check_modal" loading="lazy"></img>
-              </div>
+            <For each={props.images}>
+              {(image, index) => (
+                <div class={`carousel-item ${index() === 0 ? 'active' : ''}`}>
+                  <img
+                    src={image.src}
+                    class="card-img-top"
+                    alt={image.alt}
+                    data-bs-toggle="modal"
+                    data-bs-target={image.modalId}
+                  />
+                </div>
+              )}
+            </For>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target={props.target} data-bs-slide="prev">
+          <Show when={props.images.length > 1}>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target={`#${props.target}`}
+              data-bs-slide="prev"
+            >
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target={props.target} data-bs-slide="next">
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target={`#${props.target}`}
+              data-bs-slide="next"
+            >
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="visually-hidden">Next</span>
-          </button>
-      </div>
-      <div class="col-md-6 d-flex flex-column justify-content-center">
-        <div class="card-body">
-          <h5 class="card-title">{props.title}</h5>
-          <p class="card-text">{props.description}</p>
-          <a href={props.link} class={props.linkClass} target="_blank">{props.linkText}</a>
+            </button>
+          </Show>
         </div>
       </div>
-    </div>
-  </>
-);
-
-const CardContent3Images = ( props: CardProps ) => (
-  <>
-    <div class="timeline-date px-4 text-center">{props.date}</div>
-    <div class="card d-flex flex-column flex-md-row">
-    <div id={props.id} class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-inner">
-              <div class="carousel-item active">
-                  <img src={props.imgSrc1} class="card-img-top" alt={props.imgSrc1Alt} data-bs-toggle="modal" data-bs-target="#Kassieraktion_alle_modal" loading="lazy"></img>
-              </div>
-              <div class="carousel-item">
-                  <img src={props.imgSrc2} class="card-img-top" alt={props.imgSrc2Alt} data-bs-toggle="modal" data-bs-target="#Kassieraktion_check_modal" loading="lazy"></img>
-              </div>
-              <div class="carousel-item">
-                  <img src={props.imgSrc3} class="card-img-top" alt={props.imgSrc3Alt} data-bs-toggle="modal" data-bs-target="#Kassieraktion_check_modal" loading="lazy"></img>
-              </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target={props.target} data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target={props.target} data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-          </button>
-      </div>
       <div class="col-md-6 d-flex flex-column justify-content-center">
         <div class="card-body">
           <h5 class="card-title">{props.title}</h5>
           <p class="card-text">{props.description}</p>
-          <a href={props.link} class={props.linkClass} target="_blank">{props.linkText}</a>
+          <a href={props.link} class={props.linkClass} target="_blank">
+            {props.linkText}
+          </a>
         </div>
       </div>
     </div>
@@ -114,53 +81,63 @@ const Projekte = () => (
       <div class="timeline">
         <div class="row justify-content-center">
           <div class="col-md-10 mb-4">
-            <CardContent1Image
+          <ProjectCard
+              id='Project4'
+              target="#Project4"
+              title="„Girls Cup“ 🤾🏼‍♀️"
+              date="19.04. - 20.04.2025 | Erfurt"
+              description="Am vergangenen Sonntag fand das erste selbst organisierte Turnier des Förderverein weiblicher Handballnachwuchs Thüringen e.V. statt. Sechs Mannschaften (HC Leipzig, TUS Dieskau, HSG Saalfeld und THC) traten gegen einander an und schenkten sich dabei nichts. Die mitgereisten Fans und zahlreiche Zuschauer sorgten für ordentlich Stimmung in der „Blechbüchse“. Am Ende des Tages nahm der TUS Dieskau den Pokal 🏆 mit nach Hause. Danke an alle Vereine, die die Anreise auf sich genommen haben. Fortsetzung folgt 🖖."
+              link="https://www.betterplace.org/de/projects/141469-mit-der-handballjugend-zum-kolding-cup-2025"
+              linkText=''
+              linkClass=''
+              images={[
+                { src: "webp", alt: "Kolding", modalId: "#NewsModal6" },
+                { src: "webp", alt: "Kolding", modalId: "#NewsModal6" },
+                { src: "webp", alt: "Kolding", modalId: "#NewsModal6" },
+                { src: "webp", alt: "Kolding", modalId: "#NewsModal6" },
+              ]}
+            />
+            <ProjectCard
               id='Project1'
               target="#Project1"
               title="Internationales Handballturnier Kolding 2025"
               date="19.04. - 20.04.2025 | Erfurt"
-              imgSrc1="../_assets/images/Kolding_Cup_2025.webp"
-              imgSrc2=""  
-              imgSrc3=""
-              imgSrc1Alt="Kolding Cup 2025"
-              imgSrc2Alt=""
-              imgSrc3Alt=""
               description="Wir wollen den Mädchen der D-Jugend des THC die Teilnahme an einem der größten Handballturniere ermöglichen und uns im internationalen Vergleich mit Topteams messen."
               link="https://www.betterplace.org/de/projects/141469-mit-der-handballjugend-zum-kolding-cup-2025"
               linkText='Unterstützen'
               linkClass='btn btn-primary'
+              images={[
+                { src: "../_assets/images/Kolding_Cup_2025.webp", alt: "Kolding Cup 2025", modalId: "#NewsModal6" }
+              ]}
             />
-            <CardContent3Images
+            <ProjectCard
               id='Project2'
               target="#Project2"
               title="Erfolgreiche dm-markt Kassieraktion"
               date="11.10.2024 | Erfurt"
-              imgSrc1="../_assets/images/kassieraktion_bild_alle_2024.webp"
-              imgSrc2="../_assets/images/drogerie_markt_spende_check_2024.webp"
-              imgSrc3="../_assets/images/pdf_kassieraktion_2024.webp"
-              imgSrc1Alt="Bild: Kassieraktion"
-              imgSrc2Alt="Bild: Kassieraktion Check"
-              imgSrc3Alt="Bild: Pdf Kassieraktion"
               description="Ein großes Dankeschön geht an den dm-Markt im Thüringen Park Erfurt. In einer bisher einmaligen Aktion und mit Unterstützung der Bundesliga Profis Julie Holm & Josefine Hanfland sowie 12 Nachwuchshandballerinnen wurde in einer 2-stündigen Aktion eine unfassbare Summe von 5.700 EUR erwirtschaftet. Die Summe wird zu 100% an den Förderverein gespendet. DANKE an alle Beteiligten, die diese Aktion so erfolgreich gemacht haben."
               link=""
               linkText=''
               linkClass=''
+              images={[
+                { src: "../_assets/images/kassieraktion_bild_alle_2024.webp", alt: "Bild: Kassieraktion", modalId: "#NewsModal3" },
+                { src: "../_assets/images/drogerie_markt_spende_check_2024.webp", alt: "Bild: Kassieraktion Check", modalId: "#NewsModal4" },
+                { src: "../_assets/images/pdf_kassieraktion_2024.webp", alt: "Bild: Pdf Kassieraktion", modalId: "#NewsModal5" }
+              ]}
             />
-            <CardContent1Image
+            <ProjectCard
               id='Project3'
               target="#Project3"
               title="1. Handballcamp des Fördervereins"
               date="30.09. - 02.10.2024 | Erfurt"
-              imgSrc1="../_assets/images/Handballcamp_2024.webp"
-              imgSrc2=""  
-              imgSrc3=""
-              imgSrc1Alt="1. Handballcamp des Fördervereins"
-              imgSrc2Alt=""
-              imgSrc3Alt=""
               description="In der ersten Hälfte der Oktoberferien fand unser erstes  3-tägiges Handballcamp im Sportgymnasium Erfurt statt. Unter der Leitung von Ruben Arnold und Steffen Ahrens hatten 13 Kinder (8-11 Jahre) zahlreiche Trainingseinheiten. Es wurde nicht nur der Spaß am Handball vermittelt, sondern auch an verschieden Skills gearbeitet. Zu Gast war u.a. Nationalspielerin Kim Ott, welche den Kindern beim Training über die Schultern blickte. Danke an alle Unterstützer, die das Camp mit gefördert haben. Das nächste Camp folgt sicher."
               link=""
               linkText=''
               linkClass=''
+              images={[
+                { src: "../_assets/images/Handballcamp_2024.webp", alt: "Bild", modalId: "#ProjectModal1" },
+                { src: "webp", alt: "1. Handballcamp des Fördervereins", modalId: "#ProjectModal2" }
+              ]}
             />
           </div>
         </div>
