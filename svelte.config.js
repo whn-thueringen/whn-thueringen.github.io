3a084eb38929e3aec74744435b93b9c4feebed6d
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-11-02 12:36:08 +0100

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -18,7 +20,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	},
 	preprocess: vitePreprocess({
     scss: {
