a0a938526765c76fbb71fc4de1ce324e30dd8c53
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-11-02 12:00:14 +0100

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,6 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		kit: {
-			appDir: 'app', // Required as the default is _app
-			adapter: adapter()
-		},
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/whn-thueringen.github.io' : '',
 		}
