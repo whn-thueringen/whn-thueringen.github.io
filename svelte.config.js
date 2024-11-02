7c02c6a44e8123036b8211912fde3e43f1b98fe9
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-11-02 11:38:46 +0100

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
 
@@ -12,7 +12,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		kit: {
+			appDir: 'app', // Required as the default is _app
+			adapter: adapter()
+		},
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/whn-thueringen.github.io' : '',
 		}
