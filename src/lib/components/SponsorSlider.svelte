a43f916b1a77081a1fcb5308bd10dc114772b793
Jonas <60574217+Jodekq@users.noreply.github.com>
2024-11-14 18:28:10 +0100

    Update SponsorSlider.svelte

--- src/lib/components/SponsorSlider.svelte
+++ src/lib/components/SponsorSlider.svelte
@@ -21,7 +21,7 @@ import { base } from '$app/paths';
     },
     {
       href: "https://www.st-georg-hospital.de/",
-      imgSrc: `${base}/images/st-georg-stiftung_logo.webp`,
+      imgSrc: `${base}/images/st-georg-stiftung_logo_250.webp`,
       imgSrcAlt: "Stiftung „St.Georg-Hospital“ Delitzsch"
     }
   ];
